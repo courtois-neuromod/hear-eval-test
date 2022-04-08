7b75a6aa31e52a84502f2288eb15e78819347aaf
ltetrel <loic.tetrel.pro@gmail.com>
2022-04-08 16:58:57 -0400

    updating to isngularity 3.8; ignore .sif files instead os .simg

--- soundnetbrain_hear/containers/build_sing.bash
+++ soundnetbrain_hear/containers/build_sing.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Log-in to quay.io for docker2singularity"
+sudo docker login quay.io
 REPO_PATH=${0%/*}/../..
 echo "Removing previous container"
 rm -f ${REPO_PATH}/envs/*.simg
@@ -8,7 +10,7 @@ echo "Building docker image..."
 if sudo docker build --tag=hear-eval-kit --file Dockerfile-cuda11.2-python3.8 .; then
 	cd ../../
 	echo "Converting to singularity..."
-	sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --privileged -t --rm singularityware/docker2singularity --name hear-eval-kit hear-eval-kit
+	sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --privileged -t --rm quay.io/singularity/docker2singularity:v3.8.4 --name hear-eval-kit hear-eval-kit
 
 	if sudo docker images | grep none | awk '{ print $3; }'; then
 		echo "Deleting none images"
